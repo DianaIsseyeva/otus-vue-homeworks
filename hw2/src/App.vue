4802406e70a00b1f2db0fe240ffe4ee1ac245162
Diana <diana.dev777@gmail.com>
2024-11-18 23:12:45 +0500

    feat: implement fetch and display data

--- hw2/src/App.vue
+++ hw2/src/App.vue
@@ -1,32 +1,41 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import { onMounted, ref } from 'vue';
+import ProductItem from './components/ProductItem.vue';
+
+const items = ref([]);
+
+const fetchData =async()=> {
+  try {
+    const response = await fetch("https://fakestoreapi.com/products");
+    if (response.ok) {
+      items.value = await response.json();
+    } else {
+      console.error('Ошибка сети:', response.status);
+    }
+  } catch (error) {
+    console.error('Ошибка при получении данных:', error);
+  }
+}
+onMounted(()=> {
+  fetchData()
+})
 </script>
 
 <template>
   <div>
-    <div>
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <HelloWorld msg="Vite + Vue" />
-</div>
+    <ul>
+      <ProductItem v-for="item in items" :key='item.id' :item='item'/>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  ul {
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 20px;
+    padding: 0;
+  }
 </style>
--- hw2/src/components/HelloWorld.vue
+++ hw2/src/components/HelloWorld.vue
@@ -2,20 +2,18 @@
 import { ref } from 'vue';
 
 defineProps({
-  msg: String,
+  title: String,
 })
 
 const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h3>{{ title }}</h3>
 
 
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
