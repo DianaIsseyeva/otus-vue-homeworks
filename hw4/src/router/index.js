c8b9fa20a00c20c3251c328f7a3af26e609270f8
Diana <diana.dev777@gmail.com>
2024-11-28 18:47:33 +0500

    feat: implement navigation guards

--- hw4/src/router/index.js
+++ hw4/src/router/index.js
@@ -7,38 +7,41 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/Home.vue'),
+      meta: { requiresAuth: false },
     },
     {
       path: '/:productId',
       name: 'product',
       props: true,
       component: () => import('../views/ProductDetail.vue'),
+      meta: { requiresAuth: false },
     },
     {
       path: '/create-product',
       name: 'create-product',
       props: true,
       component: () => import('../views/CreateProduct.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/login',
       name: 'login',
       props: true,
       component: () => import('../views/Login.vue'),
+      meta: { requiresAuth: false },
     },
   ],
   scrollBehavior: () => ({ top: 0 }),
 });
 
 router.beforeEach((to, from, next) => {
-  return next();
-  // const jwt = null
-  //
-  // if (!jwt && to.name !== 'home'){
-  //   return next({ name: 'home' })
-  // }
-  //
-  // return next()
+  const isAuthorized = localStorage.getItem('isAuthorized');
+
+  if (to.meta.requiresAuth && !isAuthorized) {
+    next({ name: 'login' });
+  } else {
+    next();
+  }
 });
 
 export default router;
